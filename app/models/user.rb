6706b83ae9be04c66cc886bf22ba78b5a05506d5
kyouhei53 <kyoudesu0503@gmail.com>
2023-03-02 16:22:18 +0900

    バリデーション　エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX}
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX}
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, presence: true, format { with: VALID_NAME_KANA_REGEX}
+  validates :first_name_kana, presence: true, format { with: VALID_NAME_KANA_REGEX}
+  validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freez            
+  validates :password, format: { with: VALID_PASSWORD_REGEX}
+  
 end
