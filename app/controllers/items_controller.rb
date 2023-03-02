a871e1859b98e157573f4ffd1ad6f32b4bae59b9
kyouhei53 <kyoudesu0503@gmail.com>
2023-03-02 19:05:07 +0900

    ログイン、ログアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
+  
+  def create
+   @user = User.new(user_params)
+   if @user.save
+    redirect_to root_path
+   else
+    render 'new'
+   end
+  end
+
+  
+  
+
+   private
+   def user_params
+    params.require(:user).parmit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday)
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX}
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX}
-  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_name_kana, presence: true, format { with: VALID_NAME_KANA_REGEX}
-  validates :first_name_kana, presence: true, format { with: VALID_NAME_KANA_REGEX}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+  validates :last_name
+  validates :first_name 
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+  validates :last_name_kana 
+  validates :first_name_kana
+  end
   validates :birthday, presence: true
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freez            
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze           
   validates :password, format: { with: VALID_PASSWORD_REGEX}
   
 end
