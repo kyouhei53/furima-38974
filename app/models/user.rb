00634a6661a351e65deb38210425b6f423e43a23
kyouhei53 <kyoudesu0503@gmail.com>
2023-03-03 16:14:58 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
   validates :last_name
   validates :first_name 
   end
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
   validates :last_name_kana 
   validates :first_name_kana
   end
+
   validates :birthday, presence: true
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze           
-  validates :password, format: { with: VALID_PASSWORD_REGEX}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数字を両方含む必要があります'}
   
 end
