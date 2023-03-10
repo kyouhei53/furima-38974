63fb3c9b9a6d6531a22f81850592b27161764071
kyouhei53 <kyoudesu0503@gmail.com>
2023-03-10 14:14:28 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
@@ -49,7 +51,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if current_user != @item.user
+    @item = Item.find(params[:id])
+    if current_user != @item.user
       redirect_to action: :index
   end
+end
 end
